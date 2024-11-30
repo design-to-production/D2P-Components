b0a93d87119a86dd909928329bd642f06bc0c580
Bastian Nispel <nispel@designtoproduction.com>
2024-11-30 16:35:31 +0100

    [FEATURE] Multi Target Plugin (#20)

--- D2P_GrasshopperTools/Properties/Settings.Designer.cs
+++ D2P_GrasshopperTools/Properties/Settings.Designer.cs
@@ -12,8 +12,8 @@ namespace D2P_GrasshopperTools.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.11.0.0")]
-    public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.12.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
         
